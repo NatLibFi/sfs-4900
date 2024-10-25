9428c9e6a9dce83c009a2c5973c24d832c44e3bb
nvolk <nicholas.volk@helsinki.fi>
2024-10-25 09:07:48 +0300

    Add support for Ukrainian (#1)
    * Bug fix: replace an uppercase letter with a lowercase one in condition
    * Refactor. Add comments.
    * Support Ukrainian
    * Add tests for Ukrainian
    * Add classic Gogol (Russian) vs Hohol (Ukrainian) pair to tests
    * Update README to cover Ukrainian
    * Adjust to Melinda's coding conventions

--- test/transliteration.spec.js
+++ test/transliteration.spec.js
@@ -5,7 +5,7 @@ const mapping = [
   ['А', 'а'],['A','a'],     ['Анапа', 'Anapa'],
   ['Б', 'б'],['B','b'],     ['Бабаево', 'Babajevo'],
   ['В', 'в'],['V','v'],     ['Всеволожск', 'Vsevoložsk'],
-  ['Г', 'г'],['G','g'],     ['Гагарин', 'Gagarin'],
+  ['Г', 'г'],['G','g'],     ['Гагарин, Гоголь', 'Gagarin, Gogol'],
   ['Д', 'д'],['D','d'],     ['Дедовск', 'Dedovsk'],
   ['Е', 'е'],['E','e'],     ['Дальнереченск', 'Dalneretšensk'],
   ['E', 'e'],['Je','je'],   ['Егорьевск, Гаджиево', 'Jegorjevsk, Gadžijevo'],
@@ -39,7 +39,48 @@ const mapping = [
   ['Ь', 'ь'],['',''],       ['Козьмодемьянск', 'Kozmodemjansk'],
   ['Э', 'э'],['E','e'],     ['Энгельс', 'Engels'],
   ['Ю', 'ю'],['Ju','ju'],   ['Юрюзань', 'Jurjuzan'],
-  ['Я', 'я'],['Ja','ja'],   ['Ярославль', 'Jaroslavl']
+  ['Я', 'я'],['Ja','ja'],   ['Ярославль', 'Jaroslavl'],
+  
+];
+
+const mappingUkr = [
+  ['А', 'а'],['A','a'],	    ['Алушта', 'Alušta'],
+  ['Б', 'б'],['B','b'],	    ['Бобринець', 'Bobrynets'],
+  ['В', 'в'],['V','v'],     ['Вишневе', 'Vyšneve'],
+  ['Г', 'г'],['H','h'],	    ['Генічеськ, Гоголь',	'Henitšesk, Hohol'],
+  ['Ґ', 'ґ'],['G','g'],     ['Ґалаґан', 'Galagan'],
+  ['Д', 'д'],['D','d'],	    ['Дніпродзержинськ', 'Dniprodzeržynsk'],
+  ['Е', 'е'],['E','e'],     ['Енергодар', 'Enerhodar'],
+  ['Є', 'є'],['Je','je'],	  ['Єнакієве', 'Jenakijeve'],
+  ['Ж', 'ж'],['Ž','ž'],     ['Житомир', 'Žytomyr'],
+  ['З', 'з'],['Z','z'],     ['Запоріжжя', 'Zaporižžja'],
+  ['И', 'и'],['Y','y'],     ['Винники', 'Vynnyky'],
+  ['І', 'і'],['I','i'],     ['Іллічівськ', 'Illitšivsk'],
+  ['Ї', 'ї'],['Ji','ji'],	  ['Ізмаїл, Їжакевич', 'Izmajil, Jižakevytš'],
+  ['Й', 'й'],['J','j'],	    ['Красноармійськ', 'Krasnoarmijsk'],
+  ['Й', 'й'],['I','i'],     ['Гайсин, Хмельницький', 'Haisyn, H\'melnytskyi'],
+  ['Й', 'й'],['-','-'],     ['Григорій', 'Hryhori'],
+  ['К', 'к'],['K','k'],     ['Красноперекопськ', 'Krasnoperekopsk'],
+  ['Л', 'л'],['L','l'],	    ['Лохвиця',	'Loh\'vytsja'],
+  ['М', 'м'],['M','m'],     ['Миколаїв', 'Mykolajiv'],
+  ['Н', 'н'],['N','n'],     ['Нетішин',	'Netišyn'],
+  ['О', 'о'],['O','o'],	    ['Острог', 'Ostroh'],
+  ['П', 'п'],['P','p'],	    ['Прип’ять', 'Prypjat'],
+  ['Р', 'р'],['R','r'],     ['Рахів',	'Rah\'iv'],
+  ['С', 'с'],['S','s'],	    ['Севастополь',	'Sevastopol'],
+  ['Т', 'т'],['T','t'],	    ['Тальне', 'Talne'],
+  ['У', 'у'],['U','u'],	    ['Умань',	'Uman'],
+  ['Ф', 'ф'],['F','f'],	    ['Феодосія', 'Feodosija'],
+  ['Х', 'х'],['H\'', 'h\''],['Христинівка', 'H\'rystynivka'],	// h ja puolilainausmerkki
+  ['Ц', 'ц'],['Ts','ts'],	  ['Цюрупинськ', 'Tsjurupynsk'],
+  ['Ч', 'ч'],['Tš','tš'],	  ['Чугуїв', 'Tšuhujiv'],
+  ['Ш', 'ш'],['Š','š'],     ['Шепетівка',	'Šepetivka'],
+  ['Щ', 'щ'],['Štš','štš'],	['Щастя',	'Štšastja'],
+  ['Ю', 'ю'],['Ju','ju'],   ['Южне', 'Južne'],
+  ['Я', 'я'],['Ja','ja'],	  ['Ялта', 'Jalta'],
+  ['Ь', 'ь'],['-','-'],	    ['Комсомольськ', 'Komsomolsk'],
+  ['’',	'’'],['', ''],  	['Куп’янськ',	'Kupjansk']	// puolilainausmerkki
+  //['Ъ', 'ъ'],['', ''],			ei esiinny nykyortografiassa];
 ];
 
 describe('Transliteration', function() {
@@ -69,6 +110,33 @@ describe('Transliteration', function() {
     });
 
   });
+
+  const testDefinitionsUkr = mappingUkr.reduce((acc, item, i) => {
+    if (i%3 === 0) {
+      acc.push({});
+    }
+
+    const current = acc[acc.length-1];
+
+    switch(i%3) {
+    case 0: current.from = item; break;
+    case 1: current.to = item; break;
+    case 2: current.word = item; break;
+    }
+    return acc;
+
+  }, []);
+
+  testDefinitionsUkr.forEach(test => {
+    it(`converts UKR ${test.from[0]} to ${test.to[0]} in ${test.word[0]} to ${test.word[1]}`, () => {
+      expect(convertToLatin(test.word[0], 'ukr').result).to.equal(test.word[1]);
+    });
+    it(`converts UKR ${test.from[1]} to ${test.to[1]} in ${test.word[0]} to ${test.word[1]}`, () => {
+      expect(convertToLatin(test.word[0], 'ukr').result).to.equal(test.word[1]);
+    });
+
+  });
+
 
   describe('when given non-cyrillic characters', function() {
     let conversion;
